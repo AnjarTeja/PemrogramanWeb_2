678c1c61bf8864d2cf23df49715ff265e0f5b5a5
AnjarTeja <anjartheza04@gmail.com>
2025-05-01 14:37:25 +0700

    Anjar Teja"

---
+++ Admin/Header.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand navbar-dark sticky-top" style="background-color: #8B0000;">
+    <div class="container-lg">
+        <a class="navbar-brand" href=""><i class="bi bi-shop-window"></i> Ayam Bakar Mang Oman</a>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <?php echo $hasil['username']; ?>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end mt-2">
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-person-circle"></i> Profile</a></li>
+                        <li><a class="dropdown-item" href="Logout"><i class="bi bi-door-open"></i> Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ Proses/Edit_order.php
@@ -0,0 +1,38 @@
+<?php
+include '../Admin/Koneksi.php';
+
+$id             = isset($_POST['id']) ? mysqli_real_escape_string($conn, htmlentities($_POST['id'])) : "";
+$pelanggan      = isset($_POST['pelanggan']) ? mysqli_real_escape_string($conn, htmlentities($_POST['pelanggan'])) : "";
+$nohp           = isset($_POST['nohp']) ? mysqli_real_escape_string($conn, htmlentities($_POST['nohp'])) : "";
+$alamat         = isset($_POST['alamat']) ? mysqli_real_escape_string($conn, htmlentities($_POST['alamat'])) : "";
+$pesanan        = isset($_POST['pesanan']) ? mysqli_real_escape_string($conn, htmlentities($_POST['pesanan'])) : "";
+$total_harga    = isset($_POST['total_harga']) ? mysqli_real_escape_string($conn, htmlentities($_POST['total_harga'])) : "";
+
+if ($id && $pelanggan && $nohp && $alamat && $pesanan && $total_harga) {
+    $update = mysqli_query($conn, "UPDATE tb_order SET
+        pelanggan = '$pelanggan',
+        nohp = '$nohp',
+        alamat = '$alamat',
+        pesanan = '$pesanan',
+        total_harga = '$total_harga'
+        WHERE id = '$id'
+    ");
+    
+    if ($update) {
+        echo "<script>
+            alert('Berhasil mengupdate order!');
+            window.location.href = '../Admin/Order';
+        </script>";
+    } else {
+        echo "<script>
+            alert('Gagal mengupdate order.');
+            window.location.href = '../Admin/Order';
+        </script>";
+    }
+} else {
+    echo "<script>
+        alert('Semua field wajib diisi!');
+        window.location.href = '../Admin/Order';
+    </script>";
+}
+?>
